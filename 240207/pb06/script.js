6f49e76e9ba3113b539006ad905afb93ea56b5f1
jbinyim12@naver.com <jbinyim12@naver.com>
2024-02-07 18:31:39 +0900

    240207

---
+++ 240207/pb06/script.js
@@ -0,0 +1,9 @@
+// let wd = $(window);
+// $('section[data-type="background"]').each(function () {
+//   let bg = $(this);
+//   wd.scroll(function () {
+//     let pos = wd.scrollTop() / bg.data("speed");
+//     let cords = "50%" + yPos + "px";
+//     bg.css({ backgroundPosition: cords });
+//   });
+// });
