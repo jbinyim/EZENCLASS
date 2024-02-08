4714d27780b856685c4d90757151b53921b0ba43
jbinyim12@naver.com <jbinyim12@naver.com>
2024-02-08 18:29:30 +0900

    240208

---
+++ 240207/pb07/script.js
@@ -0,0 +1,25 @@
+$(function () {
+  $(".scroll").click(function (e) {
+    e.preventDefault();
+    $("html, boody").animate(
+      {
+        scrollTop: $(this.hash).offset().top,
+      },
+      500
+    );
+  });
+  $(window).resize(function () {
+    $(".container").width($(window).width()).height($(window).height());
+    $(".content").each(function () {
+      $(this)
+        .css("margin-left", ($(this).width() / 2) * -1 + "px")
+        .css("margin-top", ($(this).height() / 2) * -1 + "px");
+    });
+  });
+  setTimeout(function () {
+    $(window).resize();
+  }, 0);
+  $(".container").parallaxScroll({
+    friction: 0.5,
+  });
+});
