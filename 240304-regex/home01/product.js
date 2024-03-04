7e1a8f987eae9ed98d5abdf11cceeb8c825b9692
jbinyim <jbinyim12@naver.com>
2024-03-05 02:21:52 +0900

    240305h

---
+++ 240304-regex/home01/product.js
@@ -0,0 +1,34 @@
+const products = {
+  data: [
+    {
+      id: 1,
+      name: "volun01",
+      img: "./volunteer-news01",
+      title: "솔선수범 리더십 보여준다 더벤티, 임직원",
+      date: "2024.12.19",
+    },
+    {
+      id: 2,
+      name: "volun02",
+      img: "./volunteer-news02",
+      title: "유기견 모 보호 더벤티가 앞장서겠습니다",
+      date: "2024.09.25",
+    },
+    {
+      id: 3,
+      name: "volun03",
+      img: "./volunteer-news03",
+      title: "더벤티 임직원 봉사단 더벤져스 유기동",
+      date: "2024.08.21",
+    },
+    {
+      id: 4,
+      name: "volun04",
+      img: "./volunteer-news04",
+      title: "더벤티 임직원 봉사단 더벤져스 발족",
+      date: "2024.08.09",
+    },
+  ],
+};
+
+export default products;
